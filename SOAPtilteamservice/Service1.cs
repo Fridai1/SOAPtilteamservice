c89de8d0cfadcb9d673a6d130259e6de37ea9177
Fridai <Nikolaiviby3@gmail.com>
2018-03-05 12:59:45 +0100

    Add project files.

---
+++ SOAPtilteamservice/Service1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using ClassLibraryCI;
+
+namespace SOAPtilteamservice
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public string GetName()
+        {
+            Class1 s = new Class1();
+            return s.GetName();
+        }
+    }
+}
---
+++ SOAPtilteamserviceTests/Service1Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SOAPtilteamservice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOAPtilteamservice.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetNameTest()
+        {
+            Service1 s = new Service1();
+            Assert.AreEqual("Nikolai",s.GetName());
+        }
+    }
+}
